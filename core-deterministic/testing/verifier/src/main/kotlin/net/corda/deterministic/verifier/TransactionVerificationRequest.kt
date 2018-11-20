f830f61e2ece7d6bbb389d574092b1ef27977849
szymonsztuka <szymon.sztuka@yahoo.com>
2018-11-20 11:27:29 +0000

    CORDA-2150 Non downgrade contract version - fix compilation errors

--- core-deterministic/testing/verifier/src/main/kotlin/net/corda/deterministic/verifier/TransactionVerificationRequest.kt
+++ core-deterministic/testing/verifier/src/main/kotlin/net/corda/deterministic/verifier/TransactionVerificationRequest.kt
@@ -2,7 +2,7 @@ package net.corda.deterministic.verifier
 
 import net.corda.core.contracts.Attachment
 import net.corda.core.contracts.ContractAttachment
-import net.corda.core.contracts.ContractClassName
+import net.corda.core.contracts.StateRef
 import net.corda.core.internal.DEPLOYED_CORDAPP_UPLOADER
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.serialization.SerializedBytes
@@ -21,13 +21,13 @@ class TransactionVerificationRequest(val wtxToVerify: SerializedBytes<WireTransa
         val attachmentMap = attachments
                 .mapNotNull { it as? MockContractAttachment }
                 .associateBy(Attachment::id) { ContractAttachment(it, it.contract, uploader = DEPLOYED_CORDAPP_UPLOADER) }
-        val contractAttachmentMap = emptyMap<ContractClassName, ContractAttachment>()
+        val contractAttachmentMap = emptyMap<StateRef, ContractAttachment>()
         @Suppress("DEPRECATION")
         return wtxToVerify.deserialize().toLedgerTransaction(
             resolveIdentity = { null },
             resolveAttachment = { attachmentMap[it] },
             resolveStateRef = { deps[it.txhash]?.outputs?.get(it.index) },
-            resolveContractAttachment = { contractAttachmentMap[it.contract]?.id }
+            resolveContractAttachment = { contractAttachmentMap[it] }
         )
     }
 }
